df5101a6b56046a6a0cb76e0620eb49eff879634
Liefu Liu <liuliefu@hotmail.com>
2016-01-09 22:32:26 -0800

    Initialized a tool for Uploading books.

---
+++ Tools/UploadBooks/UploadBooks/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  UploadBooks
+//
+//  Created by Liefu Liu on 1/9/16.
+//  Copyright (c) 2016 SanRenXing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
