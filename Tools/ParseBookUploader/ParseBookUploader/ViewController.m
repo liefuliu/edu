7e8977de75b1a09ce8cec35ea84b60bd801a0a7c
Liefu Liu <liuliefu@hotmail.com>
2016-01-17 10:47:43 -0800

    Print out a directory path

--- Tools/ParseBookUploader/ParseBookUploader/ViewController.m
+++ Tools/ParseBookUploader/ParseBookUploader/ViewController.m
@@ -22,4 +22,29 @@
     // Update the view, if already loaded.
 }
 
+- (IBAction)buttonClicked:(id)sender {
+    NSLog(@"button clicked");
+    [self EnumerateDir];
+}
+
+- (void) EnumerateDir {
+    NSURL *url = [NSURL URLWithString:@"file:///Users/liefuliu/src/edu/Demos/BookReaderDemo/Books/"];
+    NSError *error = nil;
+    NSArray *properties = [NSArray arrayWithObjects: NSURLLocalizedNameKey,
+                           NSURLCreationDateKey, NSURLLocalizedTypeDescriptionKey, nil];
+    
+    NSArray *array = [[NSFileManager defaultManager]
+                      contentsOfDirectoryAtURL:url
+                      includingPropertiesForKeys:properties
+                      options:(NSDirectoryEnumerationSkipsHiddenFiles)
+                      error:&error];
+    if (array != nil) {
+        // Handle the error
+        for (NSString* file in array) {
+            NSLog(@"file path: %@", file);
+        }
+    }
+
+}
+
 @end
